6e197725e61f17164bfb7972cfb03793d2327e58
Ivan Plesko <ivan.plesko@gmail.com>
2011-11-05 12:14:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProxyWeb::Application.config.secret_token = 'e15409b83a39f2b6f5aaa32eb2b581e3fbf22a3ddcc450e4690f9076700ffe7f954c12f09c9e5a6341d541db7984eab36eeaebe9a5bad918bf481e0f2f2ae8b4'
