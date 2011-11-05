7ae5241e2783bdef61f8bb8dcd2195bf91510d43
Ivan Plesko <ivan.plesko@gmail.com>
2011-11-05 22:45:02 +0100

    Add evercookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	
+	before_filter :set_variables
+	
+	def set_variables
+		@uid = get_uid_from_cookie
+		@uid_new = generate_uid
+	end
+	
+	def generate_uid
+		UUIDTools::UUID.random_create.to_s.gsub(/-/, '')
+  end
+	
+	def get_uid_from_cookie
+    cookie_val = request.cookies["__peweproxy_uid"]
+    return cookie_val ? cookie_val : nil
+  end
+	
 end
