65fdfc29f71830b51f9e833c242181cfc6e4b6e9
Michal Vallus <vallusmichal@gmail.com>
2012-01-30 14:39:31 +0100

    Translate broken pages reporter.

--- app/controllers/broken_page_reporter_controller.rb
+++ app/controllers/broken_page_reporter_controller.rb
@@ -6,17 +6,17 @@ class BrokenPageReporterController < ApplicationController
 
   def create
 		if (params[:url].empty?)
-      flash[:error] = "URL nesmie byť prázdna."
+      flash[:error] = t(:url_may_not_be_empty)
       #elsif (request.env['HTTP_VIA'] =~ /AdaptiveProxy/) == nil
       #  flash[:error] = "Only proxy users are allowed to report broken pages"
     elsif (params[:url] =~ /(http:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?/) == nil
-			flash[:error] = "Zlý formát URL adresy."
+			flash[:error] = t(:wrong_url_format)
 		elsif BrokenPages.find_by_url(params[:url]) != nil
-			flash[:error] = "O tomto probléme už vieme, ďakujeme za váš záujem."
+			flash[:error] = t(:we_are_aware_of_this_problem)
     else
       BrokenPages.create(:url => params[:url], :uid => @uid, :timestamp => Time.now, :email => params[:email], :description => params[:post][:description])
       generate_pac
-			flash[:notice] = "Problém sme zaevidovali, ďakujeme. Ak ste zadali váš email, budeme vás o zmenách informovať."
+			flash[:notice] = t(:the_problem_has_been_reported_succesfully)
     end
     redirect_to :back
   end
